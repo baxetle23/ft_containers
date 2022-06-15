0ad479f7af6fa6e6afa9c4062a2285f3e094b90b
baxetle23 <ponyatov94@gmail.com>
2022-06-15 09:25:26 +0300

    add test stack begin

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 void test_ft_vector();
+void test_ft_stack();
 
 int main() {
     
     std::cout << "Begin test ..." << std::endl;
     test_ft_vector();
+    test_ft_stack();
     std::cout << "End test!" << std::endl;
 }
---
+++ tests/test_ft_stack.cpp
@@ -0,0 +1,32 @@
+#include "ft_stack.hpp"
+
+#include <cassert>
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+
+void TestConstruct() {
+
+}
+
+void TestSize() {
+
+}
+
+void TestPushPop() {
+
+}
+
+void TestComparison() {
+
+}
+
+void test_ft_stack() {
+    TestConstruct();
+    TestSize();
+    TestPushPop();
+    TestComparison();
+    std::cout << "test stack\n";
+}
