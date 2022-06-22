d89f2e7f95d799d9cab8ed6421013713a9d8813a
Baxetle <ponyatov94@gmail.com>
2022-06-22 04:34:44 +0300

    xtree cont

--- ft_map/xtree.hpp
+++ ft_map/xtree.hpp
@@ -50,6 +50,43 @@ protected:
     Tree_val (const key_compare& Parg, allocator_type Al) : Tree_ptr<Tr>(Parg, Al), Alval(Al) {
     }
     allocator_type Alval;
+
+template <typename Tr>
+class Tree : public Tree_val<Tr> {
+public:
+    typedef Tree<Tr>        Mytree;
+    typedef Tree_val<Tr>    Mybase;
+    typedef typename Tr::key_type       key_type;
+    typedef typename Tr::Key_compare    key_compare;
+    typedef typename Tr::value_compare  value_compare;
+    typedef typename Tr::value_type     value_type;
+    typedef typename Tr::allocator_type allocator_type;
+protected:
+    typedef typename Tree_nod<Tr>::Genptr           Genptr;
+    typedef typename Tree_nod<Tr>::Node             Node;
+    enum Redbl {Red, Black};
+    typedef typename allocator_type::template
+        rebind::<Node>::other::pointer              Nodeptr;
+    typedef typename allocator_type::template
+        rebind::<Nodeptr>::other::reference         Nodepref;
+    typedef typename allocator_type::template
+        rebind::<key_type>::other::const_reference  Keyref;
+    typedef typename allocator_type::template
+        rebind::<char>::other::reference            Charref;
+    typedef typename allocator_type::template
+        rebind::<value_type>::other::reference      Vref;
+
+    static Charref Color(Nodeptr P) {
+        return ((Charref)(*P).Color);
+    }
+    static Charref Isnil(Nodeptr P) {
+        return ((Charref)(*P).Isnil);
+    }
+    static Keyref Key(Nodeptr P) {
+        return (Kfn()(Value(P)));
+    }
+       
+}
 };
 
 
