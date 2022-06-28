938f06e053de5b8c0f484164a0391e5b2d287590
baxetle23 <ponyatov94@gmail.com>
2022-06-28 16:23:04 +0300

    add tree methods

--- ft_map/xtree.hpp
+++ ft_map/xtree.hpp
@@ -2,8 +2,71 @@
 #define XTREE_H_
 
 #include "ft_iterator.hpp"
+#include <iterator>
 
 namespace ft {
+
+//--------------------
+//-----CLASS_PAIR-----
+//--------------------
+
+template <typename T1, typename T2>
+struct pair {
+    typedef T1 first_type;
+    typedef T2 second_type;
+
+    first_type  first;
+    second_type second;
+
+    pair() : first(), second() {
+    }
+
+    template<class U, class V>
+    pair (const pair<U,V>& pr) : first(pr.first), second(pr.second) {
+    }
+
+    pair (const first_type& a, const second_type& b) : first(a), second(b) {
+    }
+
+    pair& operator=(const pair<first_type, second_type>& other) {
+        if (*this == other) {
+            return *this;
+        }
+        this->second = other.second;
+        this->first = other.first;
+        return *this;
+    }
+};
+
+template<class T1, class T2>
+ft::pair<T1, T2> make_pair(T1 t, T2 u) {
+    return ft::pair<T1, T2>(t, u);
+};
+
+template<class T1, class T2>
+bool	operator==(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs){
+    return (lhs.first == rhs.first) && (lhs.second == rhs.second);
+};
+template<class T1, class T2>
+bool	operator!=(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs){
+    return lhs != rhs;
+};
+template<class T1, class T2>
+bool	operator<(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs){
+    return ((lhs.first < rhs.first) || (!(rhs.first < lhs.first) && (lhs.second < rhs.second)));
+};
+template<class T1, class T2>
+bool	operator<=(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs){
+    return !(rhs < lhs);
+};
+template<class T1, class T2>
+bool	operator>(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs){
+    return rhs < lhs;
+};
+template<class T1, class T2>
+bool	operator>=(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs){
+    return !(lhs < rhs);
+};
 
 template<typename Tr>
 class Tree_nod : public Tr {
@@ -199,6 +262,216 @@ public:
     };
 
     //class const_iterator
+    class const_iterator;
+    friend class const_iterator;
+    class const_iterator : public ft::Bidit<value_type, Dift, Ctptr, const_reference> {
+    protected:
+        Nodeptr Ptr;
+    public:
+        typedef ft::Bidit<value_type, Dift, Ctptr, const_reference> Mybase;
+        typedef typename Mybase::iterator_category                  iterator_category;
+        typedef typename Mybase::difference_type                    deffirence_type;
+        typedef typename Mybase::pointer                            pointer;
+        typedef typename Mybase::reference                          reference;
+    
+        const_iterator() : Ptr(0) {}
+        const_iterator(Nodeptr ptr) : Ptr(ptr) {}
+        const_iterator(const typename Tree<Tr>::iterator it) : Ptr(it.Mynode()) {}
+
+        const_reference operator*() const {
+            return Value(Ptr);
+        }
+
+        Ctptr operator->() const {
+            return &**this;
+        }
+        const_iterator operator++() {
+            Inc();
+            return *this;
+        }
+        const_iterator operator++(int) {
+            const_iterator tmp = *this;
+            ++*this;
+            return tmp;
+        }
+        const_iterator operator--() {
+            Dec();
+            return *this;
+        }
+        const_iterator operator--(int) {
+            const_iterator tmp = *this;
+            --*this;
+            return tmp;
+        }
+        bool operator==(const const_iterator other) const {
+            return Ptr == other.Ptr;
+        }
+        bool operator!=(const const_iterator other) const {
+            return !(*this == other);
+        }
+        void Dec() {
+            if (isnil(Ptr)) {
+                Ptr == Right(Ptr);
+            } else if (!Isnil(Left(Ptr))) {
+                Ptr = Max(Left(Ptr));
+            } else {
+                Nodeptr P;
+                while(!Isnil(P = Parent(Ptr)) && Ptr == Left(Ptr)) {
+                    Ptr = P;
+                }
+                if (!Isnil(P)) {
+                    Ptr = P;
+                }
+            }
+        }
+        void Inc() {
+            if (Isnil(Ptr)) {
+                ;
+            } else if (Isnil(Right(Ptr))) {
+                Ptr = Min(Right(Ptr));
+            } else {
+                Nodeptr P;
+                while (!Isnil(P = Parent(Ptr)) && Ptr =- Right(P)) {
+                    Ptr = P;
+                }
+                Ptr = P;
+            }
+        }
+        Nodeptr Mynode() const {
+            return Ptr;
+        }
+    };
+
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+    typedef ft::pair<iterator, bool>                    Pairib;
+    typedef ft::pair<iterator, iterator>                Pairii;
+    typedef ft::pair<const_iterator, const_iterator>    Paircc;
+
+    explicit Tree(const key_compare& Parg, const allocator_type& Al) : Mybase(Parg, Al) {
+        Init();
+    }
+    Tree(const value_type *F, const value_type *L, const key_compare& Parg, const allocator_type& Al) :
+        Mybase(Parg, Al) {
+            Init();
+            insert(F, L);
+    }
+    Tree(const Mytree& other) : Mybase(other.key_comp(), other.get_allocator()) {
+        Init();
+        Copy(other);
+    }
+    ~Tree() {
+        erase(begin(), end());
+        Freenode(Head);
+        Head = 0;
+        Size = 0;
+    }
+
+    Mytree& operator=(const Mytree& other) {
+
+    }
+    iterator begin() {
+
+    }
+    const_iterator begin() const {
+
+    }
+    iterator end() {
+
+    }
+    const_iterator end() const {
+
+    }
+    //add reverse_iterator
+
+
+    size_type size() const {
+
+    }
+    size_type max_size() const {
+
+    }
+    bool empty() const {
+
+    }
+    allocator_type get_allocator() const {
+
+    }
+    key_compare key_comp() const {
+        return comp;
+    }
+    value_compare value_comp() const {
+
+    }
+
+
+protected:
+    void Copy(const Mytree& other) {
+
+    }
+    Nodeptr Copy(Nodeptr X, Nodeptr Y) {
+
+    }
+    void Erase(Nodeptr node) {
+
+    }
+    void Init() {
+
+    }
+    iterator Insert(bool Addleft, Nodeptr Y, const value_type& v) {
+
+    }
+    Nodeptr Lbound(const key_type& Kv) const {
+
+    }
+    Nodeptr& Lmost() {
+        
+    }
+    Nodeptr& Lmost() const {
+        
+    }
+    Nodeptr& Rmost() {
+        
+    }
+    Nodeptr& Rmost() const {
+        
+    }
+    Nodeptr& Root() {
+        
+    }
+    Nodeptr& Root() const {
+        
+    }
+    void Lrotate(Nodeptr X) {
+
+    }
+    void Rrotate(Nodeptr X) {
+
+    }
+    static Nodeptr Max(Nodeptr P) {
+
+    }
+    static Nodeptr Min(Nodeptr P) {
+
+    }
+    Nodeptr Ubound(const key_type& Kv) const {
+
+    }
+    Nodeptr Buynode(Nodeptr Parg, char Carg) {
+
+    }
+    void Consval(Tptr P, const value_type& V) {
+
+    }
+    void Destval(Tptr P) {
+
+    }
+    void Freenode(Nodeptr S) {
+
+    }
+
+    Nodeptr     Head;
+    size_type   Size;
 
 };
 
